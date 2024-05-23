98433704e0babc33d07d2a0f1a3b67bc41b4da04
Aziz Madaminov <madaminovaziz@mail.ru>
2024-05-23 17:29:31 +0300

    auth page without any logic

--- client/src/Pages/AuthPage/AuthPage.tsx
+++ client/src/Pages/AuthPage/AuthPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './style.scss'
 
 interface Props {
 
@@ -6,8 +6,25 @@ interface Props {
 
 const AuthPage = (props: Props) => {
     return (
-        <div>
-            Auth
+        <div className='auth-page-wrapper'>
+            <form className='auth-form'>
+                <div className="auth-form-wrapper">
+                    <div className="login auth-el">
+                        <label>
+                            ЛОГИН
+                        </label>
+                        <input type='text' name='login' className='login auth-input'/>
+                    </div>
+                    <div className="password auth-el">
+                        <label>
+                            ПАРОЛЬ
+                        </label>
+                        <input type='password' name='password' className='password auth-input'/>
+                    </div>
+                </div>
+
+                <button className='auth-form-button'>ВОЙТИ</button>
+            </form>
         </div>
     )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { router } from './Routes/Routes';
 import { RouterProvider } from 'react-router-dom';
-
+import './global.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
